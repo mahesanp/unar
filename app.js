e068087b80e8877ab34cb82bdef85fd6268007f0
mahesanp <mahesanp789@gmail.com>
2022-07-29 16:01:16 +0530

    final commit

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.get("/about", function(req, res){
+  res.render("about-us");
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact-us");
+});
+
+app.get("/case_studies", function(req, res){
+  res.render("blog-post-list");
+});
+
+app.get("/services", function(req, res){
+  res.render("service-page");
+});
+
+app.listen(3000, '0.0.0.0', function(){
+  console.log("server listening on port 3000");
+})
